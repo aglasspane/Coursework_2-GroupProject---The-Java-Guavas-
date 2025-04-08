0aed2d6b9446258cbfcfc2552ebc36d3e463d6ec
gdunleavy11 <georgedunleavy86@gmail.com>
2025-04-08 10:30:28 +0100

    Update Workout.java
    added attributes

--- Workout.java
+++ Workout.java
@@ -1,5 +1,9 @@
 package cw2;
 
 public class Workout {
-
+  public ArrayList<String> Workout = new ArrayList<String>();
+	public ArrayList<Float> wWeight= new ArrayList<Float>();
+	public ArrayList<Integer> Reps = new ArrayList<Integer>();
+	public ArrayList<Integer> Sets = new ArrayList<Integer>();
+	public ArrayList<Integer> date = new ArrayList<Integer>();
 }
