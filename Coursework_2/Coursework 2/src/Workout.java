70f811f725bef6768935ca7f9e6283a4da7104d7
ethan <ethankane14@aol.co.uk>
2025-04-17 17:30:41 +0100

    Fixed bug on GUI line 136, added missing });.

---
+++ Coursework_2/Coursework 2/src/Workout.java
@@ -0,0 +1,18 @@
+public class Workout {
+
+    private String exercise;
+    private int duration;
+
+    public Workout (String exercise, int duration) {
+        this.exercise = exercise;
+        this.duration = duration;
+    }
+
+    public String getexercise() {
+        return exercise;
+    }
+
+    public int getduration() {
+        return duration;
+    }
+}
